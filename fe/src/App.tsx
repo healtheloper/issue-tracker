3f0b63b5544fafd4515ea66e73497abf8d7c2286
healtheloper <58503584+healtheloper@users.noreply.github.com>
2022-07-01 14:13:33 +0900

    fix: add re login logic

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components';
 
 import { useHeaderState } from '@contexts/HeaderProvider';
+import useLogin from '@hooks/useLogin';
 import Routes from '@pages/Routes';
 import GlobalStyle from '@style/GlobalStyle';
 import { darkTheme, lightTheme } from '@style/theme';
 
 export default function App() {
   const { isDarkMode } = useHeaderState();
+  const { reLogin } = useLogin();
+  reLogin();
 
   return (
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
